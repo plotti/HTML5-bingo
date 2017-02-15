9a044836510be9c8853cc12f9787350b3b5ed4dd
Thomas Ebermann <plotti@gmx.net>
2017-02-15 10:57:44 +0100

    commit

--- js/data.js
+++ js/data.js
@@ -1,5 +1,5 @@
 
-var headerText = "Bingo";
+var headerText = "Lambda Retro Bingo";
 
 var footerText = "<a href='https://github.com/jeffehobbs/HTML5-bingo/' target='_blank'>Code available on github</a>";
 
@@ -10,35 +10,35 @@ var clickSnd = new Audio("audio/click.mp3");
 var winSnd = new Audio("audio/win.mp3");
 
 var JSONBingo = {"squares": [
-        {"square": "one"},
-        {"square": "two"},
-        {"square": "three"},
-        {"square": "four"},
-        {"square": "five"},
-        {"square": "six"},
-        {"square": "seven"},
-        {"square": "eight"},
-        {"square": "nine"},
-        {"square": "ten"},
-        {"square": "eleven"},
-        {"square": "twelve"},
-        {"square": "thirteen"},
-        {"square": "fourteen"},
-        {"square": "fifteen"},
-        {"square": "sixteen"},
-        {"square": "seventeen"},
-        {"square": "eighteen"},
-        {"square": "nineteen"},
-        {"square": "twenty"},
-        {"square": "twenty one"},
-        {"square": "twenty two"},
-        {"square": "twenty three"},
-        {"square": "twenty four"},    	
-        {"square": "twenty five"},    	
-        {"square": "twenty six"},    	
-        {"square": "twenty seven"}, 
-        {"square": "twenty eight"},    	
-        {"square": "twenty nine"},    	
-        {"square": "thirty"}    	
+        {"square": "lambda"},
+        {"square": "person"},
+        {"square": "patrick"},
+        {"square": "boris"},
+        {"square": "lukasz"},
+        {"square": "plotti"},
+        {"square": "elena"},
+        {"square": "max"},
+        {"square": "work"},
+        {"square": "agile"},
+        {"square": "iterate"},
+        {"square": "spike"},
+        {"square": "backlog"},
+        {"square": "story"},
+        {"square": "estimation"},
+        {"square": "retro"},
+        {"square": "bottleneck"},
+        {"square": "architecture"},
+        {"square": "migros"},
+        {"square": "migipedia"},
+        {"square": "review"},
+        {"square": "sprint"},
+        {"square": "role"},
+        {"square": "improve"},    	
+        {"square": "code"},    	
+        {"square": "difficult"},    	
+        {"square": "focus"}, 
+        {"square": "happy"},    	
+        {"square": "sad"},    	
+        {"square": "noora"}    	
     ]
 };
